a0d58457bfcb94680ae09b1a2c1a212334b89755
gnftoxic <toxic@hailxenu.net>
2011-09-20 01:27:23 -0500

    Working version of ->fetchResults()

--- class_VirusTotal.php
+++ class_VirusTotal.php
@@ -3,15 +3,15 @@
  * VirusTotal PHP API by gnftoxic (subv3rsion)
  * Version 1.0-r1 (September 20th, 2011)
  */
-class class_VirusTotal
+class VirusTotal
 {
     var $apiKey;
-    
+
     function VirusTotal($api)
     {
         $this->apiKey = $api;
     }
-    
+
     public function scanFile($fileName)
     {
         if(!file_exists($fileName))
@@ -20,16 +20,37 @@ class class_VirusTotal
         }
         return array( 'error' => 'An unknown error occured, we have reached end of request.' );
     }
-    
+
     public function fetchResults($hash)
     {
-        
-        return null;
+        $result = $this->interact("/api/get_file_report.json", array( 'resource' => $hash));
+        return $result;
     }
-    
+
     public function fetchKey()
     {
         return $this->apiKey;
+    }
+
+    private function interact($url, $data)
+    {
+        $data = array_merge($data, array('key' => $this->apiKey));
+
+        $ch = curl_init();
+
+        foreach($data as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+        rtrim($fields_string,'&');
+
+        curl_setopt($ch, CURLOPT_URL, "https://www.virustotal.com/" . $url);
+        curl_setopt($ch, CURLOPT_POST, count($data));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+
+        return json_decode($result);
     }
 }
 ?>
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+require_once("class_VirusTotal.php");
+$vt = new VirusTotal( "insertapikey" );
+$scan = $vt->fetchResults( "md5/sha1/sha256" );
+
+$report = $scan->report;
+$scanners = $report[1];
+
+foreach($scanners AS $scanner => $result)
+{
+    if(strlen($result) > 0)
+    {
+        print "Threat detected from $scanner ($result).<br />";
+        $detected++;
+    }
+    $amount++;
+}
+
+print "<br />$detected out of $amount virus scanners found a threat.<br />";
+print "<a href='{$scan->permalink}'>VirusTotal.com Report</a>";
+
+
+print "<pre>";
+print_r($scanReport);
+print "</pre>";
+?>
